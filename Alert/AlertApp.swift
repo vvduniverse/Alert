39e42bf83fb1c043a1061ec675f2cb6f5a3f8bd1
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-26 10:38:13 +0200

    Initial Commit

---
+++ Alert/AlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertApp.swift
+//  Alert
+//
+//  Created by Vahtee Boo on 26.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct AlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
